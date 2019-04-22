52c7620be90559603f6618b10849e7ae241b872a
sedmonds <info@scottedmonds.com>
2019-04-22 15:35:50 -0500

    Adds full-screen images for background

--- views/main.js
+++ views/main.js
@@ -3,11 +3,22 @@ var html = require('choo/html')
 module.exports = function (state, emit) { 
   emit('DOMTitleChange', 'A to Z Road Inc.' )
 
-  return html`
-    <body>
-      <main>
-        
-      </main> 
+  return html`<body class="bg-image">
+    <style>
+      html, body {
+        height: 100%;
+      }
+      .bg-image {
+        background-image: url("/assets/scott-webb-51465-unsplash.jpg");  
+        height: 100%;
+
+        /* center/scale */
+        background-position: center;
+        background-repeat: none;
+        background-size: cover;
+      }
+    </style>
+    <div id="main"></div> 
    </body>
   `
 }
